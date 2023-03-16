6e25f6dffb0f5576757900f8ec8001e792d603fe
Deeps <100620410+deepti-tri@users.noreply.github.com>
2023-03-16 15:26:08 -0400

    added User Authentication

---
+++ src/main/java/org/launchcode/techjobs/persistent/models/User.java
@@ -0,0 +1,45 @@
+package org.launchcode.techjobs.persistent.models;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    @NotNull
+    private String username;
+
+    @NotNull
+    private String pwHash;
+
+    private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+    public User() {}
+
+    public User(String username, String password) {
+        this.username = username;
+        this.pwHash = encoder.encode(password);
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+
+
+    public boolean isMatchingPassword(String password) {
+        return encoder.matches(password, pwHash);
+    }
+}
